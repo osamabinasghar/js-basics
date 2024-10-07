e703a7b738a07d7037d54812db82ef6bbaa253a2
Osama Bin Asghar <Obinasghar@yahoo.com>
2024-10-07 08:58:13 +0000

    Commit

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "obinasghar@yahoo.com"
 var accountPassword = "12345"
 accountCity = "Karachi"
+let accountState;
 
 // accountId = 2 // not allowed
 accountEmail = "Obinasghar123@yahoo.com"
@@ -10,4 +11,9 @@ accountCity = "Lahore"
 
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
