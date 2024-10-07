f6162d1c429153f5cd142de5e9e1b82800f75c05
Osama Bin Asghar <Obinasghar@yahoo.com>
2024-10-07 08:44:42 +0000

    Commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "obinasghar@yahoo.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+// accountId = 2 // not allowed
+accountEmail = "Obinasghar123@yahoo.com"
+accountPassword = 2121212121
+accountCity = "Lahore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
