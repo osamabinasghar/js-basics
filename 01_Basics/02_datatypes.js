d76c2b5bb8430383ace0e6f3768a60a27f581fc9
Osama Bin Asghar <Obinasghar@yahoo.com>
2024-10-07 11:03:39 +0000

    Session 01

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // We are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // Code readability should be high
+
+console.log("Osama ");
+
+
+let name = "osama"
+let age = 32
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
