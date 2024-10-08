65bf6aae072641a670a7aa2105140dc0e3afe405
Osama Bin Asghar <Obinasghar@yahoo.com>
2024-10-08 10:14:45 +0000

    Datatypes Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// const score:number = 100 TypeScript
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsiteTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+// Reference (Non-Primitve)
+
+// Array, Objects, Functions
+
+const heros = ["Batman", "Superman", "Spiderman"]
+let myObj = {
+    name: "Osama",
+    age: 32,
+}
+
+const myFunction = function() {
+    console.log("Hello World!");    
+}
+
+console.log(typeof outsiteTemp);
+
