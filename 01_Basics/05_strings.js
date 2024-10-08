3c8a4d0f82a370ff6ff9eb74c8e74b4d6a2c5d7e
Osama Bin Asghar <Obinasghar@yahoo.com>
2024-10-08 13:19:12 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Osama"
+const repoCount = 11
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Assassin`s Creed')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0, 8)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log();
+
